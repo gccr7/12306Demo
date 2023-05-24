a365f1d5341fcc7d9d2c278b2cf993c7ca77e77d
Administrator <1185925266@qq.com>
2023-05-24 16:13:00 +0800

    "frist commit"

--- next12306/backend/src/main/java/com/next/controller/TestController.java
+++ next12306/backend/src/main/java/com/next/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
-    @RequestMapping("/test")
+    @RequestMapping("/test1")
     @ResponseBody
     public JsonData test() {
         return  JsonData.success();
